605b00cf0fca3b01537604fbb94275ac33d373a2
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-11-07 19:44:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = 'John Doe'
+const status = 'active'
+const tasks = ['Task 1', 'Task 2', 'task 3']
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p>User is {{ status }}</p>
+  <ul>
+    <li v-for="task in tasks" :key="task">
+      {{ task }}
+    </li>
+  </ul>
+</template>
